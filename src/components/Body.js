9385b1fb1b1efc3e727945a2299b8105669b90ad
kondig <digonisk@gmail.com>
2021-10-11 00:48:26 +0300

    firebase error

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
-import Sticky from 'react-stickynode';
-import scrollToComponent from 'react-scroll-to-component';
+// import Sticky from 'react-stickynode';
+// import scrollToComponent from 'react-scroll-to-component';
 import {Slider} from './slider/slider';
-import {Carousel} from './carousel/carousel';
 
 import {Service} from './card';
 
@@ -17,7 +16,7 @@ import service3 from '../images/service_technicalmgmt.jpg';
 
 const Body = (props) => (
 <div>
-  <div className="mainlogo" id="logo" ref={(section) => {logo=section;}}></div>
+  <div className="mainlogo" id="logo" ></div>
   <div className="site-page">
     {/*<Sticky enabled={true} top={50} bottomBoundary={1000} className='sticky' >
       <FontAwesome className='scrollArrowTop' onClick={() => scrollToComponent(Fleet, {offset: 0, align: 'top', duration: 2500})} name='chevron-down' data-hover='SCROLL DOWN' spin={false} size='1x' />
@@ -51,9 +50,7 @@ const Body = (props) => (
     <Service image={service1} direction={`LR`} service={'Commercial Management'}  desc={`All our vessels are fully capable to offer worldwide ship transportation through all trade routes and of a wide range of cargo types. Seaspire is fixing directly with first class Charterers such as ADMI, Dreyfus, Cofco, SIMS. It is also fixing repeatedly with operators such as Clipper & Itiro. Since 2014, our vessels have carried over 1,000,000 metric tons of all dry bulk cargo types.`} />
     <Service image={service2} direction={`RL`} service={'Operations'}  desc={`Praesent ullamco distinctio vel nemo bibendum perspiciatis minus autem odit tempore a occaecati! Lobortis pellentesque quisque. Ornare! Accusantium necessitatibus purus interdum! Tristique conubia ducimus mollis pellentesque interdum natus cupiditate, perferendis minim venenatis! Autem praesentium ornare voluptatum cubilia, molestias purus fringilla sollicitudin nostrud minim quod, modi ut at.`} />
     <Service image={service3} direction={`LR`} service={'Technical Management'}  desc={`Seaspire’s management team in close cooperation with Tide Line, have managed to lower the operating expenses of the vessels by focusing on specific technical aspects and well-planned maintenance schedules, while at the same time keeping a high-quality profile.`} />
-    
-    {/*<Carousel />*/}
-    <div className="fleet" id="Fleet" ref={(section) => {Fleet=section;}}>
+    <div className="fleet" id="Fleet" >
       <span className="fleet_title"> FLEET </span>
     </div>
     <section className="text">
